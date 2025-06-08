c70e0837e8d1d7eda9e88ed3305455c1c565a41e
Alam <alamblanco18@gmail.com>
2025-06-07 23:16:38 -0300

    primer commit

--- src/test/java/curso/PaginaDeDS/PaginaDeDsApplicationTests.java
+++ src/test/java/curso/PaginaDeDS/PaginaDeDsApplicationTests.java
@@ -2,8 +2,14 @@ package curso.PaginaDeDS;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"spring.datasource.url=jdbc:mysql://localhost:3306/paginadeds",
+		"spring.datasource.username=root",
+		"spring.datasource.password="
+})
 class PaginaDeDsApplicationTests {
 
 	@Test
